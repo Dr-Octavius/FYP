cee45ec456c0aa0ddebe785f58a1c0d166e11507
Dr-Octavius <looweesing@gmail.com>
2024-11-08 13:16:00 +0800

    feat: amend transcribe.sh to run for other whisper models as well

--- transcribe.sh
+++ transcribe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Array of model types and corresponding batch sizes and output directories
-models=("tiny" "base" "small")
-batch_sizes=(8 4 2)
+models=("medium" "large" "large-v2" "large-v3" "turbo")
+batch_sizes=(32 16 16 16 8)
 
 # Iterate over models, batch sizes, and output directories
 for i in ${!models[@]}; do
@@ -14,9 +14,9 @@ for i in ${!models[@]}; do
     python3 -m src.call_features.transcribe_whisper \
         --model "$model" 
 
-    # Run the Python script with different parameters
-    # echo "Running transcription with whisperX model=$model, batch_size=$batch_size"
-    # python3 -m src.call_features.transcribe_whisperX \
-    #     --model "$model"\
-    #     --batch_size "$batch_size" 
+    Run the Python script with different parameters
+    echo "Running transcription with whisperX model=$model, batch_size=$batch_size"
+    python3 -m src.call_features.transcribe_whisperX \
+        --model "$model"\
+        --batch_size "$batch_size" 
 done
